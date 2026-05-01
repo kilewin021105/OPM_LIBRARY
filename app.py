66eb300ed8752d05736e149a15df9936243b5fb5
Arwin Kile <kilewin021105@gmail.com>
2026-05-01 22:00:57 +0800

    fixed new one

--- app.py
+++ app.py
@@ -116,4 +116,6 @@ def api_songs_delete(song_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
